1fcfac7bfb2635972ff727dc1f166a8b5fbfcbc8
Domanick01 <domanick731@gmail.com>
2026-02-17 16:58:17 -0500

    Added the user model for holding user information, Also Added Car model for if the user is a driver we will collect the data to have the number of car seats

--- account/models.py
+++ account/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+# Fields: First Name: String, Last Name: String, Age: Integer, Driver Status: Boolean,
+# Verification Status: Boolean, Rating: Float, Car: object
+class User(models.Model):
+    FIRST_NAME = models.CharField(max_length=100)
+    LAST_NAME = models.CharField(max_length=100)
+    Username = models.CharField(max_length=100)
+    Age = models.IntegerField()
+    Email = models.EmailField()
+    Phone_Number = models.IntegerField()
+    Driver_Status = models.BooleanField(default=False)
+    Verified_Status = models.BooleanField(default=False)
+    Rating = models.FloatField()
+
+class Car(models.Model):
+    CAR_MAKE = models.CharField(max_length=100)
+    CAR_MODEL = models.CharField(max_length=100)
