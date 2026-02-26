e484d0cc20d38df9fc0c68d211a341812468eb1f
Christine Samons <christine.samons2005@gmail.com>
2026-02-25 20:37:07 -0500

    Added profile.html and fixed some bugs

--- account/urls.py
+++ account/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('success/', views.success_view, name='success'),
     path('logout/', views.logout_view, name='logout'),
     path('register/', views.register_view, name='register'),
+    path('profile/', views.profile_view, name='profile'),
     path('', views.home_view, name='home'),
 ]
